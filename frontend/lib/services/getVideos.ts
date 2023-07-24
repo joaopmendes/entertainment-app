baf5121de77cd665026a31655fc018e19a3b7e69
João Mendes <joaopedrolopesmendes@gmail.com>
2023-07-24 01:13:00 +0000

    Removed url env

--- frontend/lib/services/getVideos.ts
+++ frontend/lib/services/getVideos.ts
@@ -8,7 +8,7 @@ export const getVideos = async (isClientSide: boolean, search: string, category 
     params.append('search', search);
     params.append('category', category);
 
-    const response = await fetch(`${isClientSide ? process.env.NEXT_PUBLIC_URL : process.env.URL}/api/videos?` + params.toString(), {
+    const response = await fetch(`/api/videos?` + params.toString(), {
         method: 'GET',
         cache: 'no-store',
     });
